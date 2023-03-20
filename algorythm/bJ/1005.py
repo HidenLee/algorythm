a8ddb704b37c09e2c4d9d61e8142628b312ca613
HidenLee <hyundo1995@gmail.com>
2023-03-20 17:51:59 +0900

    0320alg

--- algorythm/bJ/1005.py
+++ algorythm/bJ/1005.py
@@ -1,18 +1,23 @@
-def totalbuildT(b):
-    if not builds[b]: 
-        return times[b-1]
-    else:
-        return times[b-1] + max([totalbuildT(a) for a in builds[b]])
-
 def totalbuildT2(b,builds):
     visited = []
     stack = builds[b][:]
+    now = b
     while stack:
-        targetbd = stack.pop()
-        if not targetbd in visited:
-            visited.append(targetbd)
-            stack.extend(builds[targetbd])
-            
+        if builds[now]: # 자식 노드 있음, 
+            if all([1 if x in visited else 0 for x in builds[now]]): # 자식 탐색 끝
+                times[now] += max([times[x] for x in builds[now]])
+                visited.append(now)
+                now = stack.pop()
+                continue
+            for nxt in builds[now]:
+                if not nxt in visited: # 탐색하지 않은 자식으로 이동
+                    stack.append(now)
+                    now = nxt
+                    break
+        else: # 자식 노드 없음 -> 부모노드로 스택팝
+            visited.append(now)
+            now = stack.pop()
+    return times[b]
 
 
 
@@ -21,10 +26,10 @@ for test_case in range(T):
     N, K = map(int,input().split())
     builds = [[] for _ in range(N+1)]
     times  = list(map(int,input().split()))
+    times = [0] + times
     for _ in range(K):
         a, b = map(int,input().split())
         builds[b].append(a)
     W = int(input())
-    rst = totalbuildT(W)
     rst = totalbuildT2(W,builds)
     print(rst)
--- algorythm/bJ/7569미완.py
+++ algorythm/bJ/7569미완.py
@@ -1,17 +1,19 @@
 delta = [(1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
 def findmax(arr):
-    if not 0 in sum(arr,[]):
+    if not 0 in sum(sum(arr,[]),[]):
         return 0
-    for i in range(M):
-        for j in range(N*H):
-            while True:
-                nx = i
-                nz, ny = divmod(j,N)
-                for dlt in delta:
-                    nx, ny, nz = nx+dlt[0], ny+dlt[1], nz+dlt[2]
-                    if not arr[ny+nz*N][nx]:
-                        pass
+    for h in range(H):
+        for j in range(N):
+            for i in range(M):
+                stack=[(i,j,h)]
+                while stack:
+                    nx, ny, nz = stack.pop()
+                    for dlt in delta:
+                        nx, ny, nz = nx+dlt[0], ny+dlt[1], nz+dlt[2]
+                        if arr[nz][ny][nx] != -1:
+                            stack.append((nx,ny,nz))
+
 
 
 
@@ -19,5 +21,6 @@ def findmax(arr):
 
 
 M, N, H = map(int,input().split())
-array = [list(map(int,input().split())) for _ in range(N*H)]
+array = [[list(map(int,input().split())) for _ in range(N)] for _ in range(H)]
+roaten = [[[0]*M for _ in range(N)] for _ in range(H)]
 findmax(array)
