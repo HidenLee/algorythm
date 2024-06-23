a6a8480f4d044b5c5ee9394d7557c35422258b71
HidenLee <hyundo1995@gmail.com>
2024-06-24 03:24:15 +0900

    0624alg

--- algorythm/bJ/1325.py
+++ algorythm/bJ/1325.py
@@ -1,65 +1,74 @@
 # https://www.acmicpc.net/problem/1325
-N, M = map(int,input().split())
-trust = [set() for _ in range(N+1)]
-ans = []
-maxSize = 0
-for _ in range(M):
-    A, B = map(int,input().split())
-    trust[B].add(A)
 
 # fail - time out
-def sol_dfs(N, trust, maxSize):
-    for idx in range(1,N+1):
-        stack = [idx]
-        visit = [False]*(N+1)
-        cnt = 0
-        while stack:
-            old = stack.pop()
-            if visit[old]:
-                continue
-            visit[old] = True
-            cnt += 1
-            for new in trust[old]:
-                if not visit[new]:
-                    stack.append(new)
-        if (maxSize <cnt):
-            maxSize = cnt
-            ans = [idx]
-        elif (maxSize == cnt):
-            ans.append(idx)
-    return ans
+def sol_dfs(node):
+    stack = [node]
+    visit = [False]*(N+1)
+    visit[node] = True
+    cnt = 0
+    while stack:
+        old = stack.pop()
+        if visit[old]:
+            continue
+        visit[old] = True
+        cnt += 1
+        for new in trust[old]:
+            if not visit[new]:
+                stack.append(new)
+    return visit.count(True)
 
 
 # pass
 from collections import deque
-def sol_bfs(N, trust, maxSize):
-    for idx in range(1, N+1):
-        dq = deque([idx])
-        visit = [False] *(N+1)
-        visit[idx] = True
-        cnt = 0
-        while dq:
-            old = dq.popleft()
-            cnt += 1
-            for nxt in trust[old]:
-                if visit[nxt]:
-                    continue
-                else:
-                    dq.append(nxt)
-                    visit[nxt] = True
-        if (maxSize <cnt):
-            maxSize = cnt
-            ans = [idx]
-        elif (maxSize == cnt):
-            ans.append(idx)
-    return ans
+def sol_bfs(node):
+    dq = deque([node])
+    visit = [False] *(N+1)
+    visit[node] = True
+    cnt = 0
+    while dq:
+        old = dq.popleft()
+        cnt += 1
+        for nxt in trust[old]:
+            if visit[nxt]:
+                continue
+            else:
+                dq.append(nxt)
+                visit[nxt] = True
+    return sum(visit)
+
+#pass
+def sol_Bffs(node):
+    q = deque()
+    q.append(node)
+    infected = [False] * (N + 1)
+    infected[node] = True
+    while q:
+        node = q.popleft()
+        for nextNode in trust[node]:
+            if infected[nextNode] == True:
+                continue
+            else:
+                q.append(nextNode)
+                infected[nextNode] = True
+
+    return infected.count(True)
 
 
+N, M = map(int,input().split())
+trust = [[] for _ in range(N+1)]
+maxSize = -1
+for _ in range(M):
+    A, B = map(int,input().split())
+    trust[B].append(A)
 
-ans1 = sol_dfs(N, trust, maxSize)
-ans2 = sol_bfs(N, trust, maxSize)
+for i in range(1,N+1):
+    temp = sol_Bffs(i)
+    if maxSize < temp:
+        maxSize = temp
+        ans = [i]
+    elif maxSize == temp:
+        ans.append(i)
 
-print(*ans1)
-print(*ans2)
+print(ans)
 
 
