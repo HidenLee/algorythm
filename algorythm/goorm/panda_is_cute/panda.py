ddfc897cd259931b4ae95add9fe16739eebd5a93
Hyundo Lee <hyundo1995@gmail.com>
2024-07-26 10:07:08 +0900

    Update panda.py

--- algorythm/goorm/panda_is_cute/panda.py
+++ algorythm/goorm/panda_is_cute/panda.py
@@ -1,28 +1,23 @@
-# -*- coding: utf-8 -*-
-# UTF-8 encoding when using korean
-N, M ,K = map(int,input().split())
+N, M, K = map(int, input().split())
 pandas = []
-xsum, ysum = (0,0)
 for _ in range(K):
-	ipt1, ipt2 = map(int, input().split())
-	pandas.append((ipt1,ipt2))
-	xsum += ipt2
-	ysum += ipt1
-cenx = xsum//K
-ceny = ysum//K
+    ipt1, ipt2 = map(int, input().split())
+    pandas.append((ipt1-1, ipt2-1))
 
-delta = [(0,1),(1,0),(0,-1),(-1,0)]
-from collections import deque
-q = deque([(ceny,cenx)])
-while(q):
-	oy, ox = q.popleft()
-	if (oy,ox) in pandas:
-		for dty,dtx in delta:
-			if 0 <= oy + dty < N and 0 <= ox +dtx < M:
-				q.append((oy+dty,ox+dtx))
-		continue
-	ans = 0
-	for py,px in pandas:
-		ans += (py-oy)**2 + (px-ox)**2
-	print(ans)
-	break
+
+# 최적의 점을 찾기 위해 거리의 제곱합을 최소화하는 점을 찾음
+min_distance_sum = 10e9
+
+# 주어진 점들을 제외한 모든 가능한 점들을 탐색
+for y in range(N):
+	for x in range(M):
+		if (y, x) not in pandas:
+			distance_sum = 0
+			for py, px in pandas:
+				distance_sum += (py - y) ** 2 + (px - x) ** 2
+			else:
+				min_distance_sum = min(min_distance_sum,distance_sum)
+if min_distance_sum == 10e9:
+	print(0)
+else:
+	print(min_distance_sum)
